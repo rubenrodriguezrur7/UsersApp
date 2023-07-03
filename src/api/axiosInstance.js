11603437c132449d0c0add16054a81ccfa65e379
ruben rodriguez <ruben.rodriguez.rur7@gmail.com>
2023-07-03 00:39:23 -0500

    new project

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "https://users-api-vig8.onrender.com",
-
+           /*"https://users-crud.academlo.tech/"*/
 });
